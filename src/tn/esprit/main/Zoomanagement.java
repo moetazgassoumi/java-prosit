cd8d43120d82550066f26ed1a87600a00a56bbbc
moetazgassoumi <moetazgassoumi3@gmail.com>
2024-10-18 08:54:47 -1100

    Ajout prosit4

---
+++ src/tn/esprit/main/Zoomanagement.java
@@ -0,0 +1,49 @@
+package tn.esprit.main;
+
+import tn.esprit.entities.Animal;
+import tn.esprit.entities.Zoo;
+
+public class Zoomanagement {
+
+                public static void main(String[] args) {
+                        Animal lion = new Animal();
+                        lion.setName("Simba");
+                        lion.setAge(8);
+                        lion.setFamily("Cats");
+                        lion.setMammal(true);
+
+                        Zoo myZoo = new Zoo("Wildlife Park", "Ariana");
+                        Zoo notMyZoo = new Zoo("WaterPark", "Siliana");
+
+
+                        Animal dog = new Animal("Canine", "Snoopy", 2, true);
+
+
+                        System.out.println(myZoo.addAnimal(lion));
+                        System.out.println(myZoo.addAnimal(dog));
+
+                        myZoo.displayAnimals();
+
+                        System.out.println(myZoo.searchAnimal(dog));
+                        Animal dog2 = new Animal("Canine", "lll", 2, true);
+                        System.out.println(myZoo.searchAnimal(dog2));
+
+//           System.out.println(myZoo.removeAnimal(dog));
+                        myZoo.displayAnimals();
+
+
+                        System.out.println(myZoo);
+
+                        myZoo.addAnimal(lion);
+                        myZoo.addAnimal(dog);
+                        myZoo.addAnimal(dog2);
+                        myZoo.displayAnimals();
+
+
+                        myZoo.setName("Belvedere Park");
+                        Zoo notMyZoo1 = Zoo.comparerZoo(myZoo, notMyZoo);
+                        System.out.println(notMyZoo1);
+
+                }
+
+}
